743f57ad703aca5f7aa3f6d00ca27c9a3cd9466f
Byers <nbyers1@iuhealth.org>
2018-11-04 21:17:57 -0500

    v3

--- app.R
+++ app.R
@@ -8,7 +8,8 @@ ui <- navbarPage(
   title = "Titanic",
   tabPanel("Data Explorer", 
   
-  column(6, plotlyOutput("survived_plot", height = 250),
+  column(6, 
+         plotOutput("survived_plot", height = 250),
          plotlyOutput("gender_plot", height = 250)),
   
   column(6, 
@@ -39,15 +40,34 @@ server <- function(input, output) {
     ggplotly(p)
   })
   
-  output$survived_plot <- renderPlotly({
+  # output$survived_plot <- renderPlotly({
+  #   data <- titanic %>%
+  #     dplyr::mutate(survived_label = ifelse(survived == 0, "No", "Yes"),
+  #                   survived_pie = 1,
+  #                   color = ifelse(survived == 0, "#F8766D", "#00BFC4"))
+  #   plot_ly(data, labels =~survived_label, values = ~survived_pie, type = 'pie',
+  #           color = ~I(color)) %>%
+  #     layout(title = 'Total Survival',
+  #            xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+  #            yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  # })
+  
+  output$survived_plot <- renderPlot({
     data <- titanic %>%
-      dplyr::mutate(survived_label = ifelse(survived == 0, "No", "Yes"),
-                    survived_pie = 1)
-    plot_ly(data, labels =~survived_label, values = ~survived_pie, type = 'pie',
-            colors = c("red", "green")) %>%
-      layout(title = 'Total Survival',
-             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+      dplyr::mutate(survived = factor(survived, 0:1, c("No", "Yes")))
+    
+    p <- ggplot(data, aes(x=factor(1), fill = survived))+
+      geom_bar(width = 1) + coord_polar(theta = "y") + theme_minimal()+
+      theme(
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.border = element_blank(),
+        panel.grid=element_blank(),
+        axis.ticks = element_blank(),
+        plot.title=element_text(size=14, face="bold"),
+        axis.text.y=element_blank()
+      )
+    p
   })
   
   output$gender_plot <- renderPlotly({
